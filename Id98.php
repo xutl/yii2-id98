b9f7d5352153555b76f4243a4dab726171a1d657
Chao Xu <xutongle@gmail.com>
2017-09-01 17:48:44 +0800

    完善

--- Id98.php
+++ Id98.php
@@ -59,40 +59,221 @@ class Id98 extends Component
     }
 
     /**
-     * Sends HTTP request.
-     * @param string $method request type.
-     * @param string $url request URL.
-     * @param array $params request params.
-     * @param array $headers additional request headers.
-     * @return object response.
-     * @throws Exception on failure.
+     * 银行卡号+姓名验证+身份证号+预留手机号
+     * @param string $bankcardNo 银行卡号
+     * @param string $name 持卡人姓名
+     * @param string $idCardNo 身份证号码
+     * @param string $tel 预留手机号
+     * @return array
      */
-    protected function sendRequest($method, $url, array $params = [], array $headers = [])
+    public function getBankcard($bankcardNo, $name, $idCardNo, $tel)
     {
-        $response = $request = $this->getHttpClient()->createRequest()
-            ->setUrl($url)
-            ->setMethod($method)
-            ->setHeaders($headers)
-            ->setData($params)
-            ->send();
-        if (!$response->isOk) {
-            throw new Exception ('Http request failed.');
+        $response = $this->getHttpClient()->get('v2/bankcard', [
+            'bankcardno' => $bankcardNo,
+            'name' => $name,
+            'idcardno' => $idCardNo,
+            'tel' => $tel,
+            'appkey' => $this->apiKey,
+            'output' => 'json',
+        ])->send();
+        $result = [];
+        if (!$response->isOk || $response->data['isok'] == 0) {
+            $result['success'] = false;
+        } else if ($response->data['isok'] == 1) {
+            $result['success'] = true;
+            $result['data'] = $response->data['code'];
         }
-        return $response;
+        return $result;
+    }
+
+    /**
+     * 银行卡号+姓名验证
+     * @param string $bankcardNo 银行卡号
+     * @param string $name 持卡人姓名
+     * @return array
+     */
+    public function getBankcardByName($bankcardNo, $name)
+    {
+        $response = $this->getHttpClient()->get('v2/bankcard', [
+            'bankcardno' => $bankcardNo,
+            'name' => $name,
+            'appkey' => $this->apiKey,
+            'output' => 'json',
+        ])->send();
+        $result = [];
+        if (!$response->isOk || $response->data['isok'] == 0) {
+            $result['success'] = false;
+        } else if ($response->data['isok'] == 1) {
+            $result['success'] = true;
+            $result['data'] = $response->data['code'];
+        }
+        return $result;
+    }
+
+    /**
+     * 银行卡号+姓名验证+电话
+     * @param string $bankcardNo 银行卡号
+     * @param string $name 持卡人姓名
+     * @param string $tel 预留手机号
+     * @return array
+     */
+    public function getBankcardByNameAndTel($bankcardNo, $name, $tel)
+    {
+        $response = $this->getHttpClient()->get('v2/bankcard', [
+            'bankcardno' => $bankcardNo,
+            'name' => $name,
+            'tel' => $tel,
+            'appkey' => $this->apiKey,
+            'output' => 'json',
+        ])->send();
+        $result = [];
+        if (!$response->isOk || $response->data['isok'] == 0) {
+            $result['success'] = false;
+        } else if ($response->data['isok'] == 1) {
+            $result['success'] = true;
+            $result['data'] = $response->data['code'];
+        }
+        return $result;
+    }
+
+    /**
+     * 银行卡号+姓名验证+身份证号
+     * @param string $bankcardNo 银行卡号
+     * @param string $name 持卡人姓名
+     * @param string $idCardNo 身份证号码
+     * @return array
+     */
+    public function getBankcardByNameAndIdCard($bankcardNo, $name, $idCardNo)
+    {
+        $response = $this->getHttpClient()->get('v2/bankcard', [
+            'bankcardno' => $bankcardNo,
+            'name' => $name,
+            'idcardno' => $idCardNo,
+            'appkey' => $this->apiKey,
+            'output' => 'json',
+        ])->send();
+        $result = [];
+        if (!$response->isOk || $response->data['isok'] == 0) {
+            $result['success'] = false;
+        } else if ($response->data['isok'] == 1) {
+            $result['success'] = true;
+            $result['data'] = $response->data['code'];
+        }
+        return $result;
+    }
+
+    /**
+     * 银行卡号+身份证验证
+     * @param string $bankcardNo 银行卡号
+     * @param string $idCardNo 身份证号码
+     * @return array
+     */
+    public function getBankcardByIdCard($bankcardNo, $idCardNo)
+    {
+        $response = $this->getHttpClient()->get('v2/bankcard', [
+            'bankcardno' => $bankcardNo,
+            'idcardno' => $idCardNo,
+            'appkey' => $this->apiKey,
+            'output' => 'json',
+        ])->send();
+        $result = [];
+        if (!$response->isOk || $response->data['isok'] == 0) {
+            $result['success'] = false;
+        } else if ($response->data['isok'] == 1) {
+            $result['success'] = true;
+            $result['data'] = $response->data['code'];
+        }
+        return $result;
+    }
+
+    /**
+     * 银行卡号+身份证+手机号
+     * @param string $bankcardNo 银行卡号
+     * @param string $idCardNo 身份证号码
+     * @param string $tel 预留手机号
+     * @return array
+     */
+    public function getBankcardByIdCardAndTel($bankcardNo, $idCardNo, $tel)
+    {
+        $response = $this->getHttpClient()->get('v2/bankcard', [
+            'bankcardno' => $bankcardNo,
+            'idcardno' => $idCardNo,
+            'tel' => $tel,
+            'appkey' => $this->apiKey,
+            'output' => 'json',
+        ])->send();
+        $result = [];
+        if (!$response->isOk || $response->data['isok'] == 0) {
+            $result['success'] = false;
+        } else if ($response->data['isok'] == 1) {
+            $result['success'] = true;
+            $result['data'] = $response->data['code'];
+        }
+        return $result;
+    }
+
+    /**
+     * 银行卡号+预留手机号
+     * @param string $bankcardNo 银行卡号
+     * @param string $tel 预留手机号
+     * @return array
+     */
+    public function getBankcardByTel($bankcardNo, $tel)
+    {
+        $response = $this->getHttpClient()->get('v2/bankcard', [
+            'bankcardno' => $bankcardNo,
+            'tel' => $tel,
+            'appkey' => $this->apiKey,
+            'output' => 'json',
+        ])->send();
+        $result = [];
+        if (!$response->isOk || $response->data['isok'] == 0) {
+            $result['success'] = false;
+        } else if ($response->data['isok'] == 1) {
+            $result['success'] = true;
+            $result['data'] = $response->data['code'];
+        }
+        return $result;
+    }
+
+    /**
+     * 手机号实名认证
+     * @param string $name 姓名
+     * @param string $cardNo 身份证号码
+     * @param string $phone 手机号
+     * @return array
+     */
+    public function getCallPhone($name, $cardNo, $phone)
+    {
+        $response = $this->getHttpClient()->get('cellphone', [
+            'name' => $name,
+            'cardno' => $cardNo,
+            'phone' => $phone,
+            'appkey' => $this->apiKey,
+            'output' => 'json',
+        ])->send();
+        $result = [];
+        if (!$response->isOk || $response->data['isok'] == 0) {
+            $result['success'] = false;
+        } else if ($response->data['isok'] == 1) {
+            $result['success'] = true;
+            $result['data'] = $response->data['code'];
+        }
+        return $result;
     }
 
     /**
      * 身份证实名认证接口
      * @param string $name 姓名
-     * @param string $cardno 身份证号码
+     * @param string $cardNo 身份证号码
      * @return array
      * @throws Exception
      */
-    public function getIdCard($name, $cardno)
+    public function getIdCard($name, $cardNo)
     {
         $response = $this->getHttpClient()->get('idcard', [
             'name' => $name,
-            'cardno' => $cardno,
+            'cardno' => $cardNo,
             'appkey' => $this->apiKey,
             'output' => 'json',
         ])->send();
